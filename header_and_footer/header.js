e2b98a559dc72fa2588997391d9d56c501e17aa2
daahny <daniel.androussenko@outlook.com>
2021-05-06 20:58:01 -0700

    Box model page and other responsive features

---
+++ header_and_footer/header.js
@@ -0,0 +1,24 @@
+document.write('<header>\
+<a href="../Index/index.html"><img src="../Images/home-icon.png" alt="Home"></a>\
+<nav>\
+        <ul>\
+            <li><a href="#">HTML</a>\
+            <ul>\
+                <li><a href="../HTML Topics/Common_Tags.html">Common Tags</a></li>\
+                <li><a href="../HTML Topics/HTML_Tables.html">HTML Tables</a></li>\
+            </ul>\
+            </li>\
+            <li><a href="#">CSS</a>\
+                <ul id="css_container">\
+                    <li class="item"><a href="../CSS Topics/CSS_Box_Model.html">Box Model</a></li>\
+                    <li class="item"><a href="../CSS Topics/CSS_Selectors.html">CSS Selectors</a></li>\
+                    <li class="item"><a href="../CSS Topics/CSS_Positioning.html">CSS Positioning</a></li>\
+                    <li class="item"><a href="../CSS Topics/Flex_Boxes.html">Flex Boxes</a></li>\
+                    <li class="item"><a href="../CSS Topics/Fonts_and_Text.html">Fonts and Text</a></li>\
+                    <li class="item"><a href="../CSS Topics/User_Input.html">User Input</a></li>\
+                </ul>\
+            </li>\
+            <li><a href="#">Javascript</a></li>\
+        </ul>\
+    </nav>\
+</header>');
